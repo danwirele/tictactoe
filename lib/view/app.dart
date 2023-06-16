9b1e28b7c1416fb7029af4ba4699dab92b4e0d3a
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-16 21:08:05 +0300

    feat: add routes

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/view/game_view/game_view.dart';
+import 'package:tictactoe/view/init_view/init_view.dart';
+import 'package:tictactoe/view/undefined_view/undefined_view.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      // home: InitView(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case InitView.name:
+            return MaterialPageRoute(
+              builder: (context) => InitView(),
+            );
+          case GameView.name:
+            return MaterialPageRoute(
+              builder: (context) => GameView(),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (context) => UndefinedView(),
+            );
+        }
+      },
+    );
   }
 }
