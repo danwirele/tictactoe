fe0624a4ea4ffad8bdebba54eccaab8d61fa4349
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-16 21:07:47 +0300

    feat: create base widgets for game and init views

---
+++ lib/view/game_view/game_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GameView extends StatelessWidget {
+  static const name = 'GameView';
+  const GameView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/view/init_view/init_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InitView extends StatelessWidget {
+  static const name = 'InitView';
+
+  const InitView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
