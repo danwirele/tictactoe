3aa25d9e7ff3cd65b5b63a8776430016142d4db6
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-16 21:28:52 +0300

    feat: add Init route

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       // home: InitView(),
+      initialRoute: InitView.name,
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case InitView.name:
