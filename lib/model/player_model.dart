8726587533cbe5560f3d7a0dac7d49c174069aed
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-17 19:42:18 +0300

    feat: add player and game methods

--- lib/model/player_model.dart
+++ lib/model/player_model.dart
@@ -1,7 +1,39 @@
+import 'package:tictactoe/model/game_model.dart';
+
 abstract class PlayerModel {
-  void putSymbol(int i, int j) {}
+  void putSymbol({
+    required int i,
+    required int j,
+    required GameModel gameModel,
+  }) {}
 }
 
-class XPlayerModel {}
+//XPlayer is -1 player
+class XPlayerModel implements PlayerModel {
+  @override
+  void putSymbol({
+    required int i,
+    required int j,
+    required gameModel,
+  }) {
+    // TODO: implement putSymbol
+    if (gameModel.gameArea[i][j] == 0) {
+      gameModel.gameArea[i][j] = -1;
+    }
+  }
+}
 
-class OPlayerModel {}
+//OPlayer is true player
+class OPlayerModel implements PlayerModel {
+  @override
+  void putSymbol({
+    required int i,
+    required int j,
+    required gameModel,
+  }) {
+    // TODO: implement putSymbol
+    if (gameModel.gameArea[i][j] == 0) {
+      gameModel.gameArea[i][j] = 1;
+    }
+  }
+}
