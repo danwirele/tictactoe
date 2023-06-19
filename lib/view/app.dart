ebfe623f3fd5037508b8603835f4b0b6e131b3e1
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-20 01:06:21 +0300

    feat: add ChangeNotifierProvider

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tictactoe/controller/game_controller.dart';
 import 'package:tictactoe/view/game_view/game_view.dart';
 import 'package:tictactoe/view/init_view/init_view.dart';
 import 'package:tictactoe/view/start_game_view/start_gane_view.dart';
@@ -9,33 +11,36 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
+    return ChangeNotifierProvider<GameController>(
+      create: (context) => GameController(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.dark,
+        ),
+        // home: InitView(),
+        initialRoute: GameView.name,
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case InitView.name:
+              return MaterialPageRoute(
+                builder: (context) => InitView(),
+              );
+            case GameView.name:
+              return MaterialPageRoute(
+                builder: (context) => GameView(),
+              );
+            case StartGameView.name:
+              return MaterialPageRoute(
+                builder: (context) => StartGameView(),
+              );
+            default:
+              return MaterialPageRoute(
+                builder: (context) => UndefinedView(),
+              );
+          }
+        },
       ),
-      // home: InitView(),
-      initialRoute: GameView.name,
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case InitView.name:
-            return MaterialPageRoute(
-              builder: (context) => InitView(),
-            );
-          case GameView.name:
-            return MaterialPageRoute(
-              builder: (context) => GameView(),
-            );
-          case StartGameView.name:
-            return MaterialPageRoute(
-              builder: (context) => StartGameView(),
-            );
-          default:
-            return MaterialPageRoute(
-              builder: (context) => UndefinedView(),
-            );
-        }
-      },
     );
   }
 }
