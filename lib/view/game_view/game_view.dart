a7c0d761bca428ef794480c0fb7cf3d3571888ae
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-20 01:06:39 +0300

    feat: add provider

--- lib/view/game_view/game_view.dart
+++ lib/view/game_view/game_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tictactoe/controller/game_controller.dart';
 import 'package:tictactoe/model/entities/board.dart';
 
 class GameView extends StatelessWidget {
@@ -7,6 +9,8 @@ class GameView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Provider.of<GameController>(context);
+
     return Scaffold(
       body: Column(
         children: [
@@ -18,46 +22,40 @@ class GameView extends StatelessWidget {
                 const Spacer(),
                 AspectRatio(
                   aspectRatio: 1 / 1,
-                  child: GridView(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      childAspectRatio: 1.1,
-                    ),
-                    children: [
-                      FieldContainer(
-                        field: Field.tick(),
-                      ),
-                      FieldContainer(
-                        field: Field.tick(),
-                      ),
-                      FieldContainer(
-                        field: Field.toe(),
-                      ),
-                      FieldContainer(
-                        field: Field.tick(),
-                      ),
-                      FieldContainer(
-                        field: Field.toe(),
-                      ),
-                      FieldContainer(
-                        field: Field.toe(),
-                      ),
-                      FieldContainer(
-                        field: Field.tick(),
-                      ),
-                      FieldContainer(
-                        field: Field.tick(),
-                      ),
-                      FieldContainer(
-                        field: Field.toe(),
-                      ),
-                    ],
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (_, __) {
+                      final keys = controller.gameModel.card.keys;
+                      return GridView(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio: 1.1,
+                        ),
+                        children: keys.map(
+                          (Board board) {
+                            final Field field =
+                                controller.gameModel.card[board]!;
+                            void onTapFieldContainer() {
+                              if (controller.gameModel.turnNum < 9)
+                                controller.putField(board);
+                            }
+
+                            return FieldContainer(
+                                field: field, onTap: onTapFieldContainer);
+                          },
+                        ).toList(),
+                      );
+                    },
                   ),
                 ),
                 const Spacer(),
               ],
             ),
+          ),
+          ElevatedButton(
+            onPressed: () => controller.resetGame(),
+            child: Text('Restart'),
           ),
           const Spacer(),
         ],
@@ -67,47 +65,54 @@ class GameView extends StatelessWidget {
 }
 
 class FieldContainer extends StatelessWidget {
-  Field field;
-  FieldContainer({
+  final Field field;
+
+  final Function() onTap;
+
+  const FieldContainer({
     required this.field,
     super.key,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return field.map(
-      none: (_) => AspectRatio(
-        aspectRatio: 1 / 1,
-        child: Container(
-          color: Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: field.map(
+        none: (_) => AspectRatio(
+          aspectRatio: 1 / 1,
+          child: Container(
+            color: Colors.white,
+          ),
         ),
-      ),
-      tick: (_) => AspectRatio(
-        aspectRatio: 1 / 1,
-        child: Container(
-          color: Colors.white,
-          child: FittedBox(
-            alignment: Alignment.center,
-            fit: BoxFit.contain,
-            child: Icon(
-              Icons.circle_outlined,
-              weight: 0.01,
-              color: Colors.black,
+        tick: (_) => AspectRatio(
+          aspectRatio: 1 / 1,
+          child: Container(
+            color: Colors.white,
+            child: const FittedBox(
+              alignment: Alignment.center,
+              fit: BoxFit.contain,
+              child: Icon(
+                Icons.circle_outlined,
+                weight: 0.01,
+                color: Colors.black,
+              ),
             ),
           ),
         ),
-      ),
-      toe: (_) => AspectRatio(
-        aspectRatio: 1 / 1,
-        child: Container(
-          color: Colors.white,
-          child: FittedBox(
-            alignment: Alignment.center,
-            fit: BoxFit.contain,
-            child: Icon(
-              Icons.close,
-              weight: 0.01,
-              color: Colors.black,
+        toe: (_) => AspectRatio(
+          aspectRatio: 1 / 1,
+          child: Container(
+            color: Colors.white,
+            child: const FittedBox(
+              alignment: Alignment.center,
+              fit: BoxFit.contain,
+              child: Icon(
+                Icons.close,
+                weight: 0.01,
+                color: Colors.black,
+              ),
             ),
           ),
         ),
