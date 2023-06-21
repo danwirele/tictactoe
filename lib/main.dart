8178bfd98ecad89967b23b153e65d3b2d6ddfb65
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-21 14:12:48 +0300

    feat: add init method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/core/services/services.dart';
 import 'package:tictactoe/view/app.dart';
 
-void main() {
+void main() async {
+  await init();
   runApp(const App());
 }
